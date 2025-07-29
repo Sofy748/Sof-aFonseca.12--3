0a157d279c57ba1d05c89ea19020f3cb1f8c1174
Sofy748 <coellolorena253@gmail.com>
2025-07-29 14:59:34 -0600

    Add files via upload

---
+++ Ejercicio.js
@@ -0,0 +1,5 @@
+document.getElementById('explorarBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('¡Bienvenido a MinimarketApp! Pronto podrás explorar todos los productos.');
+});
+
